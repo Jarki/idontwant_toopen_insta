8917464629b8bed58c903e74db67942f536e8a9a
Jarki <yaroslav.ivashko@gmail.com>
2025-07-03 23:27:50 +0300

    testing: add test for get_id_from_url

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,3 +12,16 @@ from ig_reel_downloader import utils
 ])
 def test_get_urls_from_text(url, expected):
     assert utils.get_urls_from_text(url) == expected
+
+@pytest.mark.parametrize('url, expected', [
+    ('https://www.instagram.com/reel/DFvr8JTuscr', 'DFvr8JTuscr'),
+    ('https://www.instagram.com/reel/DGcc6bHIKFY', 'DGcc6bHIKFY'),
+    ('https://www.instagram.com/reel/DJcm-GGRTJq/', 'DJcm-GGRTJq'),
+    ('https://www.instagram.com/reel/D', 'D'),
+    ('https://www.instagram.com/reel/D/', 'D'),
+    ('https://www.instagram.com/reel/D?', 'D'),
+    ('https://www.instagram.com/reel/D/123/456/', 'D'),
+    ('https://www.instagram.com/reel/D/123/456/789/', 'D'),
+])
+def test_get_id_from_url(url, expected):
+    assert utils.get_id_from_url(url) == expected
